74082eb044619ee7566bc9b1da5a1090107e5b6b
XianGomez <xiiangomezz@gmail.com>
2026-06-10 23:47:15 +0200

    feat(Receipt): Añadir metodo getOrder Total y print

--- app/src/main/java/edu/teamrocket/gallerygrub/receipt/Receipt.java
+++ app/src/main/java/edu/teamrocket/gallerygrub/receipt/Receipt.java
@@ -1,17 +1,30 @@
 package edu.teamrocket.gallerygrub.receipt;
 
-import edu.teamrocket.gallerygrub.order.Order;
+import edu.teamrocket.gallerygrub.order.Comanda;
 
 public class Receipt implements Ticket {
-    private Order order;
+    private Double total;
+    private Comanda comanda;
 
-    Receipt(Order order) {
-        this.order = order;
+    public Receipt(Comanda comanda) {
+        this.comanda = comanda;
     }
 
-    @Override
-    public void total() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'total'");
+    public Comanda getOrder() {
+        return comanda;
     }
+
+    public Double total() {
+    this.total = comanda.getPedido().entrySet().stream()
+        .mapToDouble(entry -> entry.getValue()) 
+        .sum();
+
+        return (Double) total;
+    }
+
+    public void print() {
+        comanda.display();
+        System.out.println("\tTOTAL --------> " + this.total() + "$");
+    }
+
 }

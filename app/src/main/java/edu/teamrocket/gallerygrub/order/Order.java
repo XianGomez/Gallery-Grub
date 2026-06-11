9bf7cd5073b2696d00f707f5dfe4f50c7d633caa
XianGomez <xiiangomezz@gmail.com>
2026-06-11 13:33:17 +0200

    feat(Order): Añadir metodo addItem para 3 parametros

--- app/src/main/java/edu/teamrocket/gallerygrub/order/Order.java
+++ app/src/main/java/edu/teamrocket/gallerygrub/order/Order.java
@@ -2,25 +2,33 @@ package edu.teamrocket.gallerygrub.order;
 
 import java.util.HashMap;
 import java.util.Map;
+import edu.teamrocket.gallerygrub.items.Item;
+import edu.teamrocket.gallerygrub.items.Product;
 
 public class Order implements Comanda{
 
-    private Map<String,Double> pedido = new HashMap<>();
+    private Map<String, Product> pedido = new HashMap<>();
+    
 
     public void addItem(String item, double prize) {
-        pedido.put(item, prize);
+        pedido.put(item, new Item(item, prize));
+    }
+
+    public void addItem(String item, double prize, String extra) {
+        pedido.put(item + " w/ " + extra, new Item(item, prize, extra));
+        
     }
 
     public void display() {
         System.out.println("\n\t --- ORDER ---");
         pedido.entrySet().stream()
                          .forEach(entry -> System.out.println(
-                            "\t"+ entry.getKey() + "...."+entry.getValue() + "$"));
+                            "\t"+ entry.getValue().toString()));
         
     }
 
     @Override
-    public Map<String, Double> getPedido() {
+    public Map<String, Product> getPedido() {
         return this.pedido;
     }
 

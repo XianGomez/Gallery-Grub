124191657a560166021ec3721f0a4c3b2bfbf0f3
XianGomez <xiiangomezz@gmail.com>
2026-06-11 13:33:43 +0200

    feat(Receipt): Añadir metodo setChain total con los metodos calculate

--- app/src/main/java/edu/teamrocket/gallerygrub/receipt/Receipt.java
+++ app/src/main/java/edu/teamrocket/gallerygrub/receipt/Receipt.java
@@ -1,10 +1,12 @@
 package edu.teamrocket.gallerygrub.receipt;
 
 import edu.teamrocket.gallerygrub.order.Comanda;
+import edu.teamrocket.gallerygrub.extras.Extra;
 
 public class Receipt implements Ticket {
     private Double total;
     private Comanda comanda;
+    private Extra chain;
 
     public Receipt(Comanda comanda) {
         this.comanda = comanda;
@@ -14,14 +16,24 @@ public class Receipt implements Ticket {
         return comanda;
     }
 
-    public Double total() {
-    this.total = comanda.getPedido().entrySet().stream()
-        .mapToDouble(entry -> entry.getValue()) 
-        .sum();
-
-        return (Double) total;
+    @Override
+    public void setChain(Extra chain) {
+        this.chain = chain;
     }
 
+    @Override
+    public Double total() {
+        if (chain != null) {
+            this.total = chain.calculate(comanda);
+        } else {
+            this.total = comanda.getPedido().entrySet().stream()
+                .mapToDouble(entry -> entry.getValue().getPrice()) 
+                .sum();
+        }
+        return this.total;
+    }
+
+    @Override
     public void print() {
         comanda.display();
         System.out.println("\tTOTAL --------> " + this.total() + "$");

9bb04fa9c6c5cdc915d7a152329eff530f02eb74
XianGomez <xiiangomezz@gmail.com>
2026-06-11 13:31:45 +0200

    feat(Item): Añadir metodo equals hashcode y cambiar toString para 3 parametros

--- app/src/main/java/edu/teamrocket/gallerygrub/items/Item.java
+++ app/src/main/java/edu/teamrocket/gallerygrub/items/Item.java
@@ -9,6 +9,13 @@ public class Item implements Product {
         this.name = item;
         this.price = precio;
     }
+ 
+    public Item(String item, double precio, String extra) {
+        this.name = item;
+        this.price = precio;
+        this.extra = extra;
+
+    }
 
     public String getName() {
         return name;
@@ -16,6 +23,10 @@ public class Item implements Product {
 
     public Double getPrice() {
         return price;
+    }
+
+    public String getExtra() {
+        return extra;
     }
 
     public void setName(String name) {
@@ -26,9 +37,35 @@ public class Item implements Product {
         this.price = price;
     }
 
+    public void setExtra(String extra) {
+        this.extra = extra;
+    }
+
     @Override
     public String toString() {
-        return this.getName() + "...." + this.getPrice()+"$";
+        if (extra != null && !extra.isEmpty()) {
+            double extraPrice = RetailPrice.getPrice(extra);
+            return this.getName() + " w/ " + extra + "...." + this.getPrice() + "$ + " + extraPrice + "$";
+        }
+        return this.getName() + "...." + this.getPrice() + "$";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Item item = (Item) obj;
+        return name != null && name.equals(item.name) &&
+               price != null && price.equals(item.price) &&
+               ((extra == null && item.extra == null) || (extra != null && extra.equals(item.extra)));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (extra != null ? extra.hashCode() : 0);
+        return result;
     }
     
 }
